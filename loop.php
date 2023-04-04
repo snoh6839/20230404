597c73f2e8912a0248437f76a749b92033435d63
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-04-04 16:31:46 +0900

    loop

--- loop.php
+++ loop.php
@@ -1,7 +1,64 @@
 <?php
-$arr_asso = array("a" => 1, "b" => 2, "c" => 3);
+// $arr_asso = array("a" => 1, "b" => 2, "c" => 3);
 
-foreach ($arr_asso as $key => $val) {
-    echo "$key : $val\n";
-}
+// foreach ($arr_asso as $key => $val) {
+//     echo "$key : $val\n";
+// }
+
+
+// $result = 0;
+// for ($i = 1; $i <= 100; $i++) {
+//     if ($i % 2 == 0) {
+//         $result += $i;
+//     }
+// }
+// echo $result . "\n";
+
+// $result = 0;
+// for ($i = 1; $i * 2 <= 100; $i++) {
+//     $result += $i * 2;
+// }
+// echo $result . "\n";
+
+
+// $result = 0;
+// for ($i = 2; $i <= 100; $i+=2) {
+//         $result += $i;
+// }
+// echo $result . "\n";
+
+// function sumEvenNumbers($start, $end) {
+//     $result = 0;
+//     for ($i = $start; $i <= $end; $i++) {
+//         if ($i % 2 == 0) {
+//             $result += $i;
+//         }
+//     }
+//     return $result;
+// }
+
+// echo sumEvenNumbers(1, 100);
+
+// $result = array_reduce(range(1, 100), function($carry, $item) {
+//     if ($item % 2 == 0) {
+//         $carry += $item;
+//     }
+//     return $carry;
+// });
+
+// echo $result;
+
+// $result = 0;
+// $i = 1;
+// while ($i <= 100) {
+//     if ($i % 2 == 0) {
+//         $result += $i;
+//     }
+//     $i++;
+// }
+// echo $result;
+
+// $result = (50 * (50 + 1)) / 2 * 2;
+// echo $result;
+
 ?>
